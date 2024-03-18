beb75f627a2535685958d017c6f1caf8b48fccaf
Marrypierce <142252159+Marrypierce@users.noreply.github.com>
2024-03-18 11:04:08 -0300

    vetor

--- Main.java
+++ Main.java
@@ -1,6 +1,34 @@
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        int Aluno[][]= new int [][];
-        System.out.println("Hello world!");
+    public static void main(String[]args) {
+        String[]  alunos = new String[10];
+        aluno[0] = "João";
+        aluno[1] = "Ana";
+        aluno[2] = "Bia";
+        aluno[3] = "lua";
+        aluno[4]= "lucas";;
+        aluno[5] = "otavio";
+        aluno[6] = "liza";
+        aluno[7] = "eliza";
+        aluno[8] = "amelia";
+        aluno[9] = " dap";
+
+
+        Double[] notas = new Double[10];
+         Double nota = (double) 3.9;
+         Double nota =  (double)5.9;
+         Double nota[2] = (double) 10.0;
+         Double nota[3] = (double) 5.9;
+         Double nota[4] =(double) 10.0;
+         Double nota[5] =(double) 4.7;
+         Double nota[6] =(double) 5.8;
+         Double nota[7] =(double) 6.7;
+         Double nota[8] =(double) 8.7;
+         Double nota[9] =(double) 9.0;
+
+        System.out.println("os alunos são:" + aluno + "As nota são:" + notas  );
+
     }
+
 }
