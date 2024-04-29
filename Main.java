10c18e7e365b73b12ecfa0ce70700028fff5c723
Marrypierce <142252159+Marrypierce@users.noreply.github.com>
2024-04-29 10:41:57 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,38 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //pilha
+          Queue <String> carros = new LinkedList<>();
+        carros.add("honda");
+        carros.add("empala");
+        carros.add("audi");
+        carros.add("BMW");
 
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("topo:" + carros.poll());
+        System.out.println("o elemento do topo é:" + carros.peek());
+        System.out.println(carros.isEmpty()?"não tem":"tem carro");
+        
+        //fila
+
+        Stack  <String> carrosp = new Stack<>();
+        carrosp.push("Chevrolet Onix");
+        carrosp.push("hyundai HB20");
+        carrosp.push("agen gol");
+        carrosp.push("fiat mobi");
+        System.out.println("topo da fila:" + carrosp.pop());
+        System.out.println("topo:" + carrosp.peek());
+        System.out.println(carrosp.isEmpty()?"vazia":"tem carro");
+
+
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+
     }
 }
